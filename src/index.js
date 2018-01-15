6ab608b8d0b25098856a637c38bbc813398d3124
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-15 11:00:00 +0000

    Remove npm from devDependencies, not sure why it was there. Fix reducers error.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers'
 
 import App from './components/App';
-import reducers from './reducers';
+import rootReducer from './reducers';
 
 ReactDOM.render(
   <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
@@ -14,5 +13,3 @@ ReactDOM.render(
   </Provider>, 
   document.getElementById('root')
 );
-  
-  
